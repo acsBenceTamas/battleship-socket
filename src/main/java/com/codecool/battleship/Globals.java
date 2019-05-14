0a5fcd810518872121d747145c9c2816f0dcfba3
Ács Bence Tamás <acs.bence.tamas@gmail.com>
2019-05-14 13:20:11 +0200

    Remote automatically connects back upon successful connection

--- src/main/java/com/codecool/battleship/Globals.java
+++ src/main/java/com/codecool/battleship/Globals.java
@@ -10,6 +10,11 @@ public class Globals {
     public static final double WINDOW_HEIGHT = 720;
     public static final double TILE_WIDTH = 40;
     public static final double TILE_HEIGHT = 40;
+    public final static int DEFAULT_SERVER_PORT = 55655;
+    public static int LOCAL_PORT = DEFAULT_SERVER_PORT;
+    public static int REMOTE_PORT = DEFAULT_SERVER_PORT;
+    public static String REMOTE_ADDRESS = "localhost";
+    public static boolean CLIENT_CONNECTED = false;
 
     public static void changeDirection() {
         placementDirection = (placementDirection+1) % Direction.values().length;
